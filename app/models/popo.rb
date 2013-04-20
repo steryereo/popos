5a8f31f4c96ce0a9c33ff78b2873aafccc534bd1
oskarbruening <oskar@peek.com>
2013-04-20 11:34:53 -0700

    Adding popos as a resource

--- app/models/popo.rb
+++ app/models/popo.rb
@@ -3,12 +3,12 @@ class Popo
   include Mongoid::Timestamps
 
   field :name,         type: String
+  field :description,  type: String
   field :latitude,     type: Float
   field :longitude,    type: Float
   field :address,      type: String
   field :directions,   type: String
-  field :open_time,    type: Time
-  field :close_time,   type: Time
+  field :open_times,   type: String
   field :rating,       type: Float
   field :foursquareid, type: String
   field :twitter_hash_tag, type: String
