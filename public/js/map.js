16dc65985646ad8ff4e977d9b8a64db7bbd9feae
Lou Huang <lou@codeforamerica.org>
2013-05-30 18:30:58 -0700

    Add mapbox tile layer as default, with fallback to watercolor map if map.html is passed ?m=watercolor query string.

--- public/js/map.js
+++ public/js/map.js
@@ -1,15 +1,16 @@
-var layertypea = 'watercolor';
-var watercolorlayer = new L.StamenTileLayer(layertypea);
+// Set map types
+var mapboxID = 'cdawson.map-0ymh0yul';
+var stamenMap = 'watercolor',
+    stamenLabels = 'toner-labels';
 
-var layertypeb = 'toner-labels';
-var tonerlabellayer = new L.StamenTileLayer(layertypeb);
+var mapStyle = GetQueryStringParams('m');
 
+// Set map options
 var mapOptions = {
   minZoom : 17,
   maxZoom : 17,
   zoomControl : false
 };
-var map = new L.Map('map', mapOptions);
 
 var linestyle = {
   color: 'red',
@@ -17,6 +18,23 @@ var linestyle = {
   opacity : 1.0
 };
 
+// Determine map style
+if (mapStyle == 'watercolor') {
+    // Watercolor map option
+    var watercolorurl = 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg';
+    var tonerlabellayer = new L.StamenTileLayer(stamenLabels);
+
+    var map = new L.Map('map', mapOptions);
+
+    L.tileLayer(watercolorurl).addTo(map);
+    map.addLayer(tonerlabellayer);
+}
+else {
+    // Default map style
+    var map = L.mapbox.map('map', mapboxID, mapOptions);
+}
+
+// Initiate POPOS
 var popos = document.popos();
 var points = popos.walkpath;
 
@@ -25,12 +43,19 @@ document.polyline.addTo(map);
 
 map.fitBounds(document.polyline.getBounds());
 
-var watercolorurl = 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg';
-L.tileLayer(watercolorurl).addTo(map);
-map.addLayer(tonerlabellayer);
-
 // markers and popups
 popups = popos.popups();
 
 // Set first popo
 popos.setCurrentPopos(0);
+
+function GetQueryStringParams(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+}
