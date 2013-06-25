4adc84a311a6bd5f5d3c381ac037a42863dfd003
Nicholas Echols <stere.ereo@gmail.com>
2013-06-24 21:48:12 -0700

    new site is now main site

--- public/js/map.js
+++ public/js/map.js
@@ -1,14 +1,15 @@
 // Set map types
-var mapboxID = 'cdawson.map-0ymh0yul';
+var mapboxID = 'cdawson.map-rfzl19el';
 var stamenMap = 'watercolor',
     stamenLabels = 'toner-labels';
 
 var mapStyle = GetQueryStringParams('m');
+var routeID = GetQueryStringParams('routeID');
 
 // Set map options
 var mapOptions = {
-  minZoom : 17,
-  maxZoom : 17,
+  // minZoom : 17,
+  // maxZoom : 17,
   zoomControl : false
 };
 
@@ -18,41 +19,66 @@ var linestyle = {
   opacity : 1.0
 };
 
-// Determine map style
-if (mapStyle == 'new') {
-  var map = L.mapbox.map('map', mapboxID, mapOptions);
-}
-else if (mapStyle) {
+
+if (mapStyle) {
   var map = L.mapbox.map('map', mapStyle, mapOptions);  
 }
 else {
-  // Default map style
-  // Watercolor map option
-  var watercolorurl = 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg';
-  var tonerlabellayer = new L.StamenTileLayer(stamenLabels);
-
-  var map = new L.Map('map', mapOptions);
-
-  L.tileLayer(watercolorurl).addTo(map);
-  map.addLayer(tonerlabellayer);
+  var map = L.mapbox.map('map', mapboxID, mapOptions);
 }
+// Determine map style
+// if (mapStyle == 'new') {
+//   var map = L.mapbox.map('map', mapboxID, mapOptions);
+// }
+// else if (mapStyle) {
+//   var map = L.mapbox.map('map', mapStyle, mapOptions);  
+// }
+// else {
+//   // Default map style
+//   // Watercolor map option
+//   var watercolorurl = 'http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg';
+//   var tonerlabellayer = new L.StamenTileLayer(stamenLabels);
+
+//   var map = new L.Map('map', mapOptions);
+
+//   L.tileLayer(watercolorurl).addTo(map);
+//   map.addLayer(tonerlabellayer);
+// }
 
 // Initiate POPOS
+// var initMap = function (d) {
 var popos = document.popos();
-var points = popos.walkpath;
+var points = popos.points();
+// var points = _.filter(popos.points(),function(i){ return i !== undefined; });
 
-document.polyline = L.polyline(points, linestyle);
-document.polyline.addTo(map);
+if (routeID) {
+  popos.currentRouteID = routeID;
+  var route = popos.routePoints(routeID);
+  document.polyline = L.polyline(route, linestyle);
+  document.polyline.addTo(map);
 
-window.onresize = function(){popos.centerOnPath(false)};
+window.onresize = function(){
+  var idx = document.poposindex;
+   var lat = document.routeObjs[idx].latitude;
+      var lon = document.routeObjs[idx].longitude;
+      popos.centerOnPoint(new L.LatLng(lat, lon));
+  popos.centerOnPoint(false)
+};
 // markers and popups
-popups = popos.popups();
+popos.routePopups(routeID);
 
 // Set first popo
-popos.setCurrentPopos(0);
-map.fitBounds(document.polyline.getBounds());
-popos.centerOnPath(true);
+// map.fitBounds(document.polyline.getBounds());
 
+
+// popos.centerOnPath(true);
+}
+else {
+  map.fitBounds(points);
+popos.popups();
+}
+popos.setCurrentPopos(0);
+// }
 function GetQueryStringParams(sParam) {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
@@ -62,4 +88,4 @@ function GetQueryStringParams(sParam) {
             return sParameterName[1];
         }
     }
-}
+ }
---
+++ public/old_site/popos.js
@@ -0,0 +1,138 @@
+(function() {
+  document.popos = function() {
+
+    var json = 'data/popos.json',
+        data = []
+
+    $.ajax({
+        url: json,
+        async: false,
+        dataType: 'json',
+        success: function(i) {
+            data = i;
+        }
+    });
+
+    var walkpath = [
+      new L.LatLng(37.7951184623911, -122.40214169025421),
+      new L.LatLng(37.794783580481706, -122.40209341049193),
+      new L.LatLng(37.794859882822436, -122.4014925956726),
+      new L.LatLng(37.794406306638834, -122.40141212940217),
+      new L.LatLng(37.79432576478241, -122.40227580070496),
+      new L.LatLng(37.7938679462461, -122.40219533443451),
+      new L.LatLng(37.79397816207892, -122.40132093429564),
+      new L.LatLng(37.79265556123338, -122.40103662014006),
+      new L.LatLng(37.79280817023158, -122.3998886346817),
+      new L.LatLng(37.79222316736003, -122.39974915981291),
+      new L.LatLng(37.79231218983492, -122.39914834499358),
+      new L.LatLng(37.79277849628437, -122.39926099777222),
+      new L.LatLng(37.792816648500015, -122.3987030982971),
+      new L.LatLng(37.793342299242944, -122.39880502223969),
+      new L.LatLng(37.79337621206568, -122.39857971668242),
+      new L.LatLng(37.792659800376455, -122.39749073982237),
+      new L.LatLng(37.79351186320097, -122.39761412143706),
+      new L.LatLng(37.793723817601254, -122.39749073982237),
+    ];
+
+    var poposMarker = {
+      default : L.icon({
+        iconUrl: 'img/poposmarkerblue.png',
+        shadowUrl: 'img/marker-shadow.png',
+        iconAnchor: new L.Point(23, 60)
+      }),
+      selected : L.icon({
+        iconUrl: 'img/poposmarkerred.png',
+        shadowUrl: 'img/marker-shadow.png',
+        iconAnchor: new L.Point(23, 60)
+      })
+    };
+
+    document.popos = [];
+
+    $('.left-arrow').on('click', function() {
+      if (document.poposindex >= 1) {
+        setCurrentPopos( document.poposindex - 1 );
+      }
+    });
+
+    $('.right-arrow').click(function() {
+      if (document.poposindex >= 0 && document.poposindex < document.popos.length - 1) {
+        setCurrentPopos( document.poposindex + 1 );
+      }
+    });
+
+    var centerOnPath = function(reset) {
+      // this should move the center of the polyline to the center of the "non-detailview" map area
+      reset = typeof reset !== 'undefined' ? reset : false; //default argument reset = false
+      var clearAreaLeft = $("#detailview").width() + $("#detailview").offset().left;
+      var clearAreaCenter = Math.floor(window.innerWidth - (window.innerWidth - clearAreaLeft) / 2);
+      var centerOffset = clearAreaCenter - Math.floor(window.innerWidth / 2);
+
+      // var z = map.getZoom();
+      var z = 17; // Hardcoded - BAD!
+      var pathCenterAbs = map.project(document.polyline.getBounds().getCenter(), z);
+      var newCenterLatLng = map.unproject([pathCenterAbs.x - centerOffset, pathCenterAbs.y]);
+      map.setView(newCenterLatLng, z, reset);
+    };
+
+    var setCurrentPopos = function(idx) {
+      document.poposindex = idx;
+
+      if (document.poposindex + 1 == document.popos.length) {
+        $('.right-arrow img').css('visibility', 'hidden');
+      } else {
+        $('.right-arrow img').css('visibility', 'visible');
+      }
+
+      if (document.poposindex <= 0) {
+        $('.left-arrow img').css('visibility', 'hidden');
+      } else {
+        $('.left-arrow img').css('visibility', 'visible');
+      }
+
+      var popo = document.popos[idx];
+
+      // reset other popos icons
+      _.forEach(document.popos, function(popo) {
+        popo.setIcon(poposMarker.default);
+      });
+
+      // set default icon
+      popo.setIcon(poposMarker.selected);
+
+      // display content
+      var m_place = $('#m_place').html();
+      $('#place').html(Mustache.render(m_place, data[idx]));
+
+      $('#detailview').draggable({ handle: '#place .title', containment: '#map', cursor: '-webkit-grabbing !important'});
+
+      //map.fitBounds(document.polyline.getBounds());
+      centerOnPath();
+
+    };
+
+    return {
+      data : data,
+      points : function() {
+        return _.map(data, function(d) {
+          return new L.LatLng(d.latitude, d.longitude);
+        });
+      },
+      popups : function() {
+
+        _.forEach(data, function(d,index) {
+
+          var marker = L.marker([d.latitude, d.longitude], { icon: poposMarker.default });
+          document.popos[index] = marker;
+          marker.addTo(map);
+          marker.on('click', function(e) {
+            setCurrentPopos(index);
+          });
+        });
+      },
+      walkpath : walkpath,
+      setCurrentPopos : setCurrentPopos,
+      centerOnPath : centerOnPath 
+    };
+  }
+})();
