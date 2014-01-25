f1c0204eab40f4f13d0ca99819baa0e5aa33eb7d
Nick Echols <stere.ereo@gmail.com>
2014-01-25 12:52:37 -0800

    still messing with precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Urbanwander::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-   config.assets.precompile += %w( places.js jquery.js jquery_ujs.js jquery.Jcrop.js)
+   config.assets.precompile += %w( places.js jquery.js jquery_ujs.js jquery.Jcrop.js jquery.Jcrop.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
