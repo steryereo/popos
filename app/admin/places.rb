250fb628481ccca3d6ba5765dd1d2d53f7824a38
Nick Echols <stere.ereo@gmail.com>
2014-01-25 14:34:03 -0800

    slight change

--- app/admin/places.rb
+++ app/admin/places.rb
@@ -128,7 +128,7 @@ ActiveAdmin.register Place do
     column :name
     column :address
     column "Photo" do |place|
-      link_to(image_tag(place.photo_url(:thumbnail), :height => '100'), admin_place_path(place))
+      link_to(image_tag(place.photo_url(:thumb), :height => '100'), admin_place_path(place))
     end
     # column "Photo Url", :url_for_photo
     column :category
