6f850aadc9890d88baeb18b6fe9df4c560854e10
Nick Echols <stere.ereo@gmail.com>
2015-06-04 14:50:34 -0700

    delete fucking aws keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,10 +6,6 @@ CarrierWave.configure do |config|
 
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    # :provider               => 'AWS',       # required
-    # :aws_access_key_id      => 'AKIAIRYL4J7LLLBVSFYA',       # required
-    # :aws_secret_access_key  => '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',       # required
-    # :region                 => 'us-west-1'  # optional, defaults to 'us-east-1'
     provider:               'AWS',      # required
     aws_access_key_id:      ENV["AWS_ACCESS_KEY_ID"],       # required
     aws_secret_access_key:  ENV["AWS_SECRET_ACCESS_KEY"],       # required
